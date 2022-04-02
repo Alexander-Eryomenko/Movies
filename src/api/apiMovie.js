b66518ff836c3899dc9c375f1d1b5e284cfb27ff
Alexander Yeryomenko <a.a.eryomenko@gmail.com>
2022-04-02 13:41:06 +0300

    add env file

--- src/api/apiMovie.js
+++ src/api/apiMovie.js
@@ -1,15 +1,15 @@
 import axios from 'axios'
 
-export class ApiMovies {
-  static apiKey = '0b4531aff274b4a3d4d6602140c1450b';
-  static url = `https://api.themoviedb.org/3/movie/popular?api_key=${ApiMovies.apiKey}`
+export class MoviesApi {
+  static apiKey = process.env.VUE_APP_API_KEY;
+  static url = `https://api.themoviedb.org/3/movie/popular?api_key=${MoviesApi.apiKey}`
   static urlDetailsMovie = 'https://api.themoviedb.org/3/movie/'
 
   static getTopMovies () {
-    return axios.get(ApiMovies.url).then(response => response.data.results)
+    return axios.get(MoviesApi.url).then(response => response.data.results)
   }
 
   static getDetailsOfMovie (id) {
-    axios.get(`${ApiMovies.urlDetailsMovie}${id}?api_key=${ApiMovies.apiKey}`).then(response => response.data)
+    axios.get(`${MoviesApi.urlDetailsMovie}${id}?api_key=${MoviesApi.apiKey}`).then(response => response.data)
   }
 }
