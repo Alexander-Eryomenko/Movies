920bfc1ab08d2dc089752fe2c3be87650592a43d
Alexander Yeryomenko <a.a.eryomenko@gmail.com>
2022-04-19 16:24:06 +0300

    fixed classes for header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="app-header">
-    <div class="header">
-      <router-link class="header__logo" to="/">
-        <img class="header__logo-img" src="@/assets/logo.svg" alt="logo">
+    <div class="app-header__container">
+      <router-link class="app-header__logo" to="/">
+        <img class="app-header__logo-img" src="@/assets/logo.svg" alt="logo">
       </router-link>
-      <div class="header__nav">
-        <v-btn to="/raiting-movies" x-small class="header__nav__btn">Raiting films</v-btn>
-        <v-btn to="/some-movies" x-small class="header__nav__btn">Some films</v-btn>
+      <div class="app-header__nav">
+        <v-btn to="/raiting-movies" x-small class="app-header__nav__btn">Raiting films</v-btn>
+        <v-btn to="/some-movies" x-small class="app-header__nav__btn">Some films</v-btn>
       </div>
     </div>
   </div>
@@ -23,12 +23,12 @@ export default {
   background-color: green;
   width: 100%;
   min-height: 100px;
-}
-.header {
-  height: 100%;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
+    &__container {
+      height: 100%;
+      display: flex;
+      justify-content: space-around;
+      align-items: center;
+    }
     &__logo {
       height: 50%;
       width: 100px;
