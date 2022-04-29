e5ba1378a724c8c8528dfa833f88790b8d68b03d
Alexander Yeryomenko <a.a.eryomenko@gmail.com>
2022-04-29 13:43:02 +0300

    fix rebase

--- src/views/DetailsAboutMovieView.vue
+++ src/views/DetailsAboutMovieView.vue
@@ -4,41 +4,38 @@
       <span>
         {{ dataAboutMovie.original_title }}
       </span>
-    </div>
-    <div v-if="dataAboutMovie" class="descr">
-      <img class="descr__img" :src="'https://image.tmdb.org/t/p/w500' + dataAboutMovie.backdrop_path" alt="moviePicture">
-      <div>
-      <img v-if="dataAboutMovie.backdrop_path" class="descr__img" :src="'https://image.tmdb.org/t/p/w500' + dataAboutMovie.backdrop_path" alt="moviePicture">
-      <img v-if="dataAboutMovie.backdrop_path" class="descr__img" :src="'https://image.tmdb.org/t/p/w500' + dataAboutMovie.backdrop_path" alt="moviePicture">
-      <div v-if="dataAboutMovie.budget">
-        <span>Budget: </span>
-        <span>{{ dataAboutMovie.budget }} $</span>
       </div>
-      <div v-if="dataAboutMovie.homepage">
-        <span>Home page: </span>
-        <a class="descr__link" target="_blank" :href="dataAboutMovie.homepage">{{dataAboutMovie.original_title}}</a>
+      <div v-if="dataAboutMovie" class="descr">
+        <img v-if="dataAboutMovie.backdrop_path" class="descr__img" :src="'https://image.tmdb.org/t/p/w500' + dataAboutMovie.backdrop_path" alt="moviePicture">
+        <div v-if="dataAboutMovie.budget">
+          <span>Budget: </span>
+          <span>{{ dataAboutMovie.budget }} $</span>
+        </div>
+        <div v-if="dataAboutMovie.homepage">
+          <span>Home page: </span>
+          <a class="descr__link" target="_blank" :href="dataAboutMovie.homepage">{{dataAboutMovie.original_title}}</a>
+        </div>
+        <div>
+          <span>Release date: </span>
+          <span>{{ dataAboutMovie.release_date }}</span>
+        </div>
+        <div v-if="dataAboutMovie.revenue">
+          <span>Revenue: </span>
+          <span>{{ dataAboutMovie.revenue }} $</span>
+        </div>
+        <div>
+          <span>Runtime: </span>
+          <span>{{ dataAboutMovie.runtime }} minutes</span>
+        </div>
+        <div>
+          <span>Status: </span>
+          <span>{{ dataAboutMovie.status }}</span>
+        </div>
+        <div>
+          <span>Tagline: </span>
+          <span>{{ dataAboutMovie.tagline }}</span>
+        </div>
       </div>
-      <div>
-        <span>Release date: </span>
-        <span>{{ dataAboutMovie.release_date }}</span>
-      </div>
-      <div v-if="dataAboutMovie.revenue">
-        <span>Revenue: </span>
-        <span>{{ dataAboutMovie.revenue }} $</span>
-      </div>
-      <div>
-        <span>Runtime: </span>
-        <span>{{ dataAboutMovie.runtime }} minutes</span>
-      </div>
-      <div>
-        <span>Status: </span>
-        <span>{{ dataAboutMovie.status }}</span>
-      </div>
-      <div>
-        <span>Tagline: </span>
-        <span>{{ dataAboutMovie.tagline }}</span>
-      </div>
-    </div>
   </v-container>
 </template>
 
@@ -52,7 +49,7 @@ export default {
     }
   },
   created () {
-    this.$store.dispatch('getDatailsOfTopMovie', this.id)
+    this.$store.dispatch('getDatailsOfMovie', this.id)
   },
   destroyed () {
     this.$store.commit('destroyMoviesData', {
